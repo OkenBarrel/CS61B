d05492a614134ee0222f9c9ada05293b16f3614d
Milky <theshiretherivendell@outlook.com>
2023-07-05 18:21:26 +0800

    deleted all .imls

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -11,10 +11,25 @@ public class SquarePrimesTest {
      * is broken. Write your own JUnit Test to try to uncover the bug!
      */
     @Test
-    public void testSquarePrimesSimple() {
+    public void testSquarePrimesSimpleOne() {
         IntList lst = IntList.of(14, 15, 16, 17, 18);
         boolean changed = IntListExercises.squarePrimes(lst);
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesSimpleTwo() {
+        IntList lst = IntList.of(1, 2, 3, 5, 11);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 4 -> 9 -> 25 -> 121", lst.toString());
+        assertTrue(changed);
+    }
+    @Test
+    public void testSuperPrinesSimplwThree(){
+        IntList lst=IntList.of(1,2,3,6,11,12);
+        boolean changed=IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 4 -> 9 -> 6 -> 121 -> 12",lst.toString());
         assertTrue(changed);
     }
 }
